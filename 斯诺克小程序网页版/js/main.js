655850b66d3ff792cb1b59cf3dbf4eeb1313a3d3
513419123@qq.com <513419123@qq.com>
2017-08-23 12:14:35 +0800

    修改tab方式，分离出页面、css、js。

---
+++ 斯诺克小程序网页版/js/main.js
@@ -0,0 +1,13 @@
+//比分tab绑定点击事件
+$("#tab1").bind("click",function(){
+	self.location='../discovery/discovery.html';
+});
+//赛事tab绑定点击事件
+$("#tab2").bind("click",function(){
+	self.location='../match/match.html';
+});
+
+//世界排名tab绑定点击事件
+$("#tab3").bind("click",function(){
+	self.location='../ranking/ranking.html';
+});
